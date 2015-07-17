98196a77343364d6bb8ef923b60ae18ae6120a51
Kevin Shi <im@shikev.in>
2015-07-17 00:26:34 -0400

    tables working

--- GUI/deckGUI.cc
+++ GUI/deckGUI.cc
@@ -19,18 +19,9 @@ DeckGUI::DeckGUI()  {
         for (unsigned rank = ACE; rank<= KING; ++rank) {
             string filename = "cards_png/" + to_string(suit) + "_" + to_string(rank) + ".png";
             image_names[suit][rank] = filename.c_str();
-            cout << image_names[suit][rank] << endl;
             deck.push_back(createPixbuf(image_names[suit][rank]));
         }
     }
-	// Images can only be loaded once the main window has been initialized, so cannot be done as a static
-	// constant array. Instead, use the STL transform algorithm to apply the method createPixbuf to every
-	// element in the array of image names, starting with first and ending with the last. New elements are
-	// added to the back of deck.
-    // jfor (unsigned suit = CLUB; suit <= SPADE; ++suit) {
-    //     transform( &image_names[suit][ACE], &image_names[suit][KING], 
-    //                std::back_inserter(deck), &createPixbuf );
-    // }
     deck.push_back(createPixbuf("cards_png/nothing.png"));
 } // DeckGUI::DeckGUI
 
@@ -39,7 +30,7 @@ DeckGUI::~DeckGUI() {
 
 // Returns the image for the specified card.
 Glib::RefPtr<Gdk::Pixbuf> DeckGUI::getCardImage( Card card ) {
-	int index = ((int) card.getSuit())*4 + ((int) card.getRank() );
+	int index = card.getSuit()*13 + card.getRank();
 	return deck[ index ];
 } // DeckGUI::getCardImage 
 
